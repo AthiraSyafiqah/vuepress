7776a12fca753bc9ee77ffebcea9c15399a07af2
CC18381@stdmail.ump.edu.my <CC18381@stdmail.ump.edu.my>
2020-10-02 11:01:50 +0800

    create project and nav bar

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    title: 'Coursetro',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'About me', link: 'http://github.com'},
+        ]
+    }
+}
